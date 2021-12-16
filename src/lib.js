37ceb2728cee24d38988f5c236a90cdb7cb31608
Alex Lunyov <isuntc@gmail.com>
2021-12-16 12:10:33 +0200

    Add exports

--- src/lib.js
+++ src/lib.js
@@ -6,6 +6,12 @@ export const tokens = allTokens;
 
 export { default as BaseButton } from '../src/components/base-button.vue';
 export { default as BaseHeading } from '../src/components/base-heading.vue';
+export { default as BaseText } from '../src/components/base-text.vue';
+export { default as BaseSpace } from '../src/components/base-space.vue';
 export { default as PopupLayout } from '../src/components/popup-layout.vue';
 export { default as DropdownLayout } from '../src/components/dropdown-layout.vue';
+export { default as BaseDropdown } from '../src/components/base-dropdown.vue';
+export { default as BaseSelect } from '../src/components/base-select.vue';
+export { default as BaseInput } from '../src/components/base-input.vue';
+export { default as FormField } from '../src/components/form-field.vue';
 export { default as FormFooter } from '../src/components/form-footer.vue';
