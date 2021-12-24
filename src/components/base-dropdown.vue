3ccc4e3a16bcc97addd0d3846adbb6c26928a5aa
Alex Lunyov <isuntc@gmail.com>
2021-12-24 16:43:11 +0200

    Basic integration of positioning engine

--- src/components/base-dropdown.vue
+++ src/components/base-dropdown.vue
@@ -2,7 +2,7 @@
   <div :class="{ [$style.dropdown]: true, [$style.naked]: naked }">
     <div ref="triggerPlaceholder"><slot :show="show" :on-toggle="handleToggle" /></div>
     <div ref="menuPlaceholder">
-      <div v-if="show" :class="$style.menu" :style="menuStyle">
+      <div v-if="show" ref="menu" :class="$style.menu" :style="menuStyle">
         <slot name="menu" :show="show" :on-toggle="handleToggle" />
       </div>
     </div>
@@ -11,7 +11,10 @@
 
 <script>
 import { nanoid } from 'nanoid';
+import { spaceXs } from '../tokens/tokens';
+import { computePosition, flip, offset } from '@floating-ui/dom';
 import layers from '../libs/layers';
+
 
 export default {
   name: 'HuntKitDropdown',
@@ -45,7 +48,39 @@ export default {
       };
     },
   },
+  watch: {
+    show(value) {
+      if (value) {
+        this.$nextTick(() => {
+          this.updatePosition();
+        });
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.updatePosition, { passive: true });
+    window.addEventListener('resize', this.updatePosition);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.updatePosition);
+    window.removeEventListener('resize', this.updatePosition);
+  },
   methods: {
+    updatePosition() {
+      const trigger = this.$refs.triggerPlaceholder.childNodes[0];
+      const menu = this.$refs.menu;
+      if (trigger && menu) {
+        computePosition(trigger, menu, {
+          placement: 'bottom-start',
+          strategy: 'fixed',
+          middleware: [flip(), offset(parseInt(spaceXs))]
+        }).then(({ x, y }) => {
+          console.log(x, y);
+          menu.style.left = `${x}px`;
+          menu.style.top = `${y}px`;
+        });
+      }
+    },
     handleToggle() {
       if (this.show) {
         this.close();
@@ -81,11 +116,11 @@ export default {
 }
 
 .menu {
-  position: absolute;
+  position: fixed;
   z-index: 1;
   left: 0;
   top: 100%;
-  margin-top: var(--spaceXs);
+  /* margin-top: var(--spaceXs); */
 }
 
 .dropdown:not(.naked) .menu {
