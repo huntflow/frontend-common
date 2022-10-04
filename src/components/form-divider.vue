897fe393a8ebcb6a95921bdcd9f5ddd69f78d667
Alex Lunyov <isuntc@gmail.com>
2022-10-04 16:26:34 +0300

    Divider

---
+++ src/components/form-divider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.divider" />
+</template>
+
+<script>
+export default {
+  name: 'HKFormDivider'
+}
+</script>
+
+<style module>
+.divider {
+  margin: 30px 0 20px;
+  height: 1px;
+
+  background-color: #e5e4ec;
+}
+</style>
--- src/components/form-field.stories.js
+++ src/components/form-field.stories.js
@@ -1,4 +1,5 @@
 import FormField from './form-field.vue';
+import FormDivider from './form-divider.vue';
 import BaseInput from './base-input.vue';
 
 export default {
@@ -12,17 +13,25 @@ export default {
 };
 
 const Template = (args, { argTypes }) => ({
-  components: { FormField, BaseInput },
+  components: { FormDivider, FormField, BaseInput },
   props: Object.keys(argTypes),
   data() {
     return args;
   },
   template: `
+  <div>
   <form-field v-bind="$data" v-on="$listeners">
     <template #default="{ id }">
       <base-input :id="id" />
     </template>
   </form-field>
+  <form-divider />
+  <form-field v-bind="$data" v-on="$listeners">
+    <template #default="{ id }">
+      <base-input :id="id" />
+    </template>
+  </form-field>
+  </div>
   `,
 });
 
