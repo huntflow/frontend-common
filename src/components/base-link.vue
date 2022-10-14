1b1511bc5d549897c71b28b144588af45e50da37
Alex Lunyov <isuntc@gmail.com>
2022-10-14 13:41:27 +0300

    Link

---
+++ src/components/base-link.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :class="$style.link" v-bind="$attrs" v-on="$listeners">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'HuntKitLink'
+};
+</script>
+
+<style module>
+.link {
+  font-size: 16px;
+  line-height: 150%;
+  font-family: var(hunt-kit-graphik);
+
+  color: #1DA1B5;
+  text-decoration: none;
+}
+
+.link:hover {
+  color: #148B9D;
+}
+</style>
